3cca65edb2b3ed4b11ff07f68e24a10e6b673284
tere <guilhermeterenciani@gmail.com>
2021-08-26 14:39:16 -0400

    Incluindo as imagens no header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Text>Oi</Text>
+                <Image source={
+                    require("./src/assets/arrow-left.png")
+                    }>
+                </Image>
+                <Image source={
+                    require("./src/assets/ei_search.png")
+                    }>
+                </Image>
+                <Image source={
+                    require("./src/assets/share.png")
+                    }>
+                </Image>
+                
             </View>
             <Text>Hello World!</Text>
             <StatusBar style="auto"  />
@@ -18,7 +30,7 @@ const styles = StyleSheet.create({
     container: {
     },
     header:{
-        height:93,
+        minHeight:93,
         backgroundColor: "#2C4877",
         paddingTop:40,
     },
