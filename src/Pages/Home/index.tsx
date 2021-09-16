05ffcf682b0e0f2d2dbd1bc9c63fbcc7f9bc00fe
Guilherme Terenciani <guilhereterenciani@gmail.com>
2021-09-16 10:09:36 -0400

    Separando estilos em arquivos separados dos componentes.

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import {Text, View, Image, TextInput } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+
+import {stylesLink,stylesLinkImportantes,styles} from './styles';
 
 const LinkItem = (props:any)=>{
     return(
         <Text style={stylesLink.text}>{props.name}</Text>
     );
 }
-const stylesLink = StyleSheet.create({
-    text:{
-        //backgroundColor:"red",
-        marginTop:10,
-        color: '#4A4A4C',
-        fontSize: 13.71,
-
-    }
-});
-
-
 const LinksImportantes = ()=>{
     return(//Aqui dentro ficará o nosso JSX
         <View style={stylesLinkImportantes.container}>
@@ -29,19 +20,6 @@ const LinksImportantes = ()=>{
         </View>
     );
 };
-const stylesLinkImportantes = StyleSheet.create({
-    container:{
-        flexDirection: "row",
-        justifyContent:"space-between",
-        paddingHorizontal:40,
-        paddingVertical:10,
-
-        borderBottomColor:"#F0F0F0",
-        borderBottomWidth:1,
-
-    }
-});
-
 const Home = () => {
     return(
         <View style={styles.container}>
@@ -73,33 +51,4 @@ const Home = () => {
         </View>
     );
 }
-const styles = StyleSheet.create({
-    container: {
-        flex:1,
-    },
-    header:{
-        minHeight:93,
-        backgroundColor: "#4369B0",
-        paddingTop:40,
-        flexDirection:"row",
-        justifyContent:"space-between",
-        paddingHorizontal:30,
-    },
-    inputTextView:{
-        flex:1,
-        backgroundColor:"#2C4877",
-        height:40,
-        marginHorizontal:10,
-        alignItems: "center",
-        borderRadius:28,
-        flexDirection:"row"
-    },
-    textInput:{
-        color:"#F5FFFF",
-        // maxWidth:200
-        flex:1,
-        paddingRight:10,
-    },
-});
-
 export default Home;
